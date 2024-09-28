dc5a29994a3f5e683a6f1fbdfe99678580e9d88d
User <shadilcheriyil98@gmail.com>
2024-09-28 10:33:50 +0530

    home

--- lib/featurs/details adding/screen/local.dart
+++ lib/featurs/details adding/screen/local.dart
@@ -466,8 +466,6 @@ class _localState extends State<local> {
                             code:prepaidCategory[index]["ProviderCode"],
                             dash:prepaidCategory[index]["dash"],
                             iso:prepaidCategory[index]["CountryIso"]
-
-
                           ),));
                         },
                         child: Container(
